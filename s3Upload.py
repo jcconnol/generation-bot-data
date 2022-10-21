68cef9dbc4639efacd42f7bacd65bb4ef762396c
John Connolly <jcconnol4@gmail.com>
2022-10-21 17:46:30 -0500

    fix encode bug

--- s3Upload.py
+++ s3Upload.py
@@ -9,7 +9,7 @@ def s3Upload(bucket_name, object_path, upload_text):
     
     s3_resource = boto3.resource("s3")
     print(upload_text)
-    s3_object_body = upload_text.encode('ascii')
+    s3_object_body = upload_text.encode('utf-8', errors='ignore').encode('ascii')
 
     upload_result = s3_resource.Object(bucket_name, object_path).put(Body=s3_object_body)
     print(upload_result)
